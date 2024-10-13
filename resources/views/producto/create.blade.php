7f83d5ba4cbd78d00bac2a794b623bd530bfcc2e
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-10-13 18:36:22 -0300

    finish proveedores module succesfull, db and front

--- resources/views/producto/create.blade.php
+++ resources/views/producto/create.blade.php
@@ -8,6 +8,26 @@
     #descripcion {
         resize: none;
     }
+
+    /* Estilos para los selects */
+    .bootstrap-select .dropdown-toggle {
+        background-color: white; /* Fondo blanco para el botón del select */
+        color: black; /* Color del texto del botón */
+        border-color: #d1d3e2;
+    }
+
+    /* .bootstrap-select .dropdown-menu {
+        background-color: white;
+        color: black; 
+    } */
+
+    .bootstrap-select .dropdown-item {
+        color: black; 
+    }
+
+    .bootstrap-select .dropdown-item:hover {
+        background-color: #f8f9fa; 
+    }
 </style>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
@@ -25,88 +45,90 @@
         <li class="breadcrumb-item active">Crear producto</li>
     </ol>
 
-    <div class="container w-100 border rounded p-4 mt-5">
+    <div class="card text-bg-light">
         <form action="{{ route('productos.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
-            <div class="row g-3">
-                <!-- codigo -->
-                <div class="col-md-6 mb-2">
-                    <label for="codigo" class="form-label">Código:</label>
-                    <input type="text" class="form-control" id="codigo" name="codigo" value="{{ old('codigo') }}">
-                    @error('codigo')
-                    <small class="text-danger">{{ '*'.$message }}</small>
-                    @enderror
-                </div>
-                <!-- nombre -->
-                <div class="col-md-6 mb-2">
-                    <label for="nombre" class="form-label">Nombre:</label>
-                    <input type="text" class="form-control" id="nombre" name="nombre" value="{{ old('nombre') }}">
-                    @error('nombre')
-                    <small class="text-danger">{{ '*'.$message }}</small>
-                    @enderror
-                </div>
+            <div class="card-body">
+                <div class="row g-3">
+                    <!-- codigo -->
+                    <div class="col-md-6 mb-2">
+                        <label for="codigo" class="form-label">Código:</label>
+                        <input type="text" class="form-control" id="codigo" name="codigo" value="{{ old('codigo') }}">
+                        @error('codigo')
+                        <small class="text-danger">{{ '*'.$message }}</small>
+                        @enderror
+                    </div>
+                    <!-- nombre -->
+                    <div class="col-md-6 mb-2">
+                        <label for="nombre" class="form-label">Nombre:</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre" value="{{ old('nombre') }}">
+                        @error('nombre')
+                        <small class="text-danger">{{ '*'.$message }}</small>
+                        @enderror
+                    </div>
 
-                <!-- descripcion -->
-                <div class="col--md-12">
-                    <label for="nombre" class="form-label">Descripcion:</label>
-                    <textarea name="descripcion" id="descripcion" cols="30" rows="6" class="form-control"> {{ old('descripcion') }}</textarea>
-                    @error('descripcion')
-                    <small class="text-danger">{{ '*'.$message }}</small>
-                    @enderror
-                </div>
+                    <!-- descripcion -->
+                    <div class="col--md-12">
+                        <label for="nombre" class="form-label">Descripcion:</label>
+                        <textarea name="descripcion" id="descripcion" cols="30" rows="6" class="form-control"> {{ old('descripcion') }}</textarea>
+                        @error('descripcion')
+                        <small class="text-danger">{{ '*'.$message }}</small>
+                        @enderror
+                    </div>
 
-                <!-- Fecha Vencimiento -->
-                <div class="col-md-6 mb-2">
-                    <label for="fecha_vencimiento" class="form-label">Fecha de Vencimiento:</label>
-                    <input type="date" class="form-control" id="fecha_vencimiento" name="fecha_vencimiento" value="{{ old('fecha_vencimiento') }}">
-                    @error('fecha_vencimiento')
-                    <small class="text-danger">{{ '*'.$message }}</small>
-                    @enderror
-                </div>
+                    <!-- Fecha Vencimiento -->
+                    <div class="col-md-6 mb-2">
+                        <label for="fecha_vencimiento" class="form-label">Fecha de Vencimiento:</label>
+                        <input type="date" class="form-control" id="fecha_vencimiento" name="fecha_vencimiento" value="{{ old('fecha_vencimiento') }}">
+                        @error('fecha_vencimiento')
+                        <small class="text-danger">{{ '*'.$message }}</small>
+                        @enderror
+                    </div>
 
-                <!-- imagen -->
-                <div class="col-md-6 mb-2">
-                    <label for="img_path" class="form-label">Imagen:</label>
-                    <input type="file" class="form-control" id="img_path" name="img_path" accept="Image/*">
-                    @error('img_path')
-                    <small class="text-danger">{{ '*'.$message }}</small>
-                    @enderror
-                </div>
+                    <!-- imagen -->
+                    <div class="col-md-6 mb-2">
+                        <label for="img_path" class="form-label">Imagen:</label>
+                        <input type="file" class="form-control" id="img_path" name="img_path" accept="Image/*">
+                        @error('img_path')
+                        <small class="text-danger">{{ '*'.$message }}</small>
+                        @enderror
+                    </div>
 
-                <!-- marca select -->
-                <div class="col-md-6 mb-2">
-                    <label for="marca_id" class="form-label">Marca:</label>
-                    <select data-size="5" title="Seleccione una marca..." data-live-search="true" name="marca_id" id="marca_id" class="form-control selectpicker">
-                        @foreach ($marcas as $item)
-                        <option value="{{ $item->id }}" {{old('marca_id') == $item->id ? 'selected' : '' }} >
-                            {{ $item->nombre }}
-                        </option>
-                        @endforeach
-                    </select>
-                    @error('marca_id')
-                    <small class="text-danger">{{ '*'.$message }}</small>
-                    @enderror
-                </div>
+                    <!-- marca select -->
+                    <div class="col-md-6 mb-2">
+                        <label for="marca_id" class="form-label">Marca:</label>
+                        <select data-size="5" title="Seleccione una marca..." data-live-search="true" name="marca_id" id="marca_id" class="form-control selectpicker">
+                            @foreach ($marcas as $item)
+                            <option value="{{ $item->id }}" {{old('marca_id') == $item->id ? 'selected' : '' }} >
+                                {{ $item->nombre }}
+                            </option>
+                            @endforeach
+                        </select>
+                        @error('marca_id')
+                        <small class="text-danger">{{ '*'.$message }}</small>
+                        @enderror
+                    </div>
 
 
-                <!-- categoria select -->
-                <div class="col-md-6 mb-2">
-                    <label for="categorias" class="form-label">Categorías:</label>
-                    <select data-size="5" title="Seleccione una categoria..." data-live-search="true" name="categorias[]" id="categorias" class="form-control selectpicker" multiple>
-                        @foreach ($categorias as $item)
-                        <option value="{{ $item->id }}" {{ (in_array($item->id, old('categorias', [])) ? 'selected' : '')}} >
-                            {{ $item->nombre }}
-                        </option>
-                        @endforeach
-                    </select>
-                    @error('categorias')
-                    <small class="text-danger">{{ '*'.$message }}</small>
-                    @enderror
-                </div>
+                    <!-- categoria select -->
+                    <div class="col-md-6 mb-2">
+                        <label for="categorias" class="form-label">Categorías:</label>
+                        <select data-size="5" title="Seleccione una categoria..." data-live-search="true" name="categorias[]" id="categorias" class="form-control selectpicker" multiple>
+                            @foreach ($categorias as $item)
+                            <option value="{{ $item->id }}" {{ (in_array($item->id, old('categorias', [])) ? 'selected' : '')}} >
+                                {{ $item->nombre }}
+                            </option>
+                            @endforeach
+                        </select>
+                        @error('categorias')
+                        <small class="text-danger">{{ '*'.$message }}</small>
+                        @enderror
+                    </div>
 
-                <!-- botones -->
-                <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Guardar</button>
+                    <!-- botones -->
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary">Guardar</button>
+                    </div>
                 </div>
 
 
--- resources/views/proveedore/index.blade.php
+++ resources/views/proveedore/index.blade.php
@@ -1,13 +1,76 @@
 @extends('template')
 
 @section('title','proveedores')
-
+<!-- 
 @push('css-datatable')
 <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" type="text/css">
-@endpush
+@endpush -->
 
 @push('css')
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" type="text/css">
+
+<style>
+    .custom-badge {
+        background-color: #2ecc71;
+        border-color: #a9dfbf;
+        color: #000;
+        border-radius: 0.35rem;
+        padding: 0.375rem 0.75rem;
+        font-size: 1rem;
+        display: inline-block;
+        vertical-align: middle;
+    }
+
+    .custom-badge-delete {
+        background-color: #e74c3c;
+        border-color: #f5b7b1;
+        color: #000;
+        border-radius: 0.35rem;
+        padding: 0.375rem 0.75rem;
+        font-size: 1rem;
+        display: inline-block;
+        vertical-align: middle;
+    }
+
+    .btn-pastel-yellow,
+    .btn-pastel-blue,
+    .btn-pastel-red,
+    .btn-pastel-green {
+        border-radius: 0.35rem;
+        padding: 0.375rem 0.75rem;
+        font-size: 1rem;
+        display: inline-block;
+        vertical-align: middle;
+    }
+
+    .btn-pastel-yellow {
+        background-color: #f1c40f;
+        border-color: #f9e79f;
+        color: #000;
+    }
+
+    .btn-pastel-blue {
+        background-color: #6a9bdc;
+        border-color: #85c1ae;
+        color: #000;
+    }
+
+    .btn-pastel-red {
+        background-color: #e74c3c;
+        border-color: #f5b7b1;
+        color: #000;
+    }
+
+    .btn-pastel-green {
+        background-color: #2ecc71;
+        border-color: #a9dfbf;
+        color: #000;
+    }
+</style>
+
+
 @endpush
 
 @section('content')
@@ -36,7 +99,7 @@
 
 
 <div class="container-fluid px-4">
-    <h1 class="mt-4 mb-4 fw-bold"  style="font-size: 3rem;">Proveedores</h1>
+    <h1 class="mt-4 mb-4 fw-bold" style="font-size: 3rem;">Proveedores</h1>
     <ol class="breadcrumb mb-4">
         <li class="breadcrumb-item"><a href="{{ route('panel') }}">Inicio</a></li>
         <li class="breadcrumb-item active">Proveedores</li>
@@ -51,116 +114,99 @@
 
 
     <div class="card mb-4"">
-        <div class="card-header">
-            <i class="fas fa-table me-1"></i>
-            Tabla proveedores
-        </div>
-        <div class="card-body">
-            <table id="datatablesSimple" class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Dirección</th>
-                        <th>Documento</th>
-                        <th>Tipo de persona</th>
-                        <th>Estado</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($proveedores as $item)
-                    <tr>
-                        <td>
-                            {{$item->persona->razon_social}}
-                        </td>
-                        <td>
-                            {{$item->persona->direccion}}
-                        </td>
-                        <td>
-                            <p class="fw-semibold mb-1">{{$item->persona->documento->tipo_documento}}</p>
-                            <p class="text-muted mb-0">{{$item->persona->numero_documento}}</p>
-                        </td>
-                        <td>
-                            {{$item->persona->tipo_persona}}
-                        </td>
-                        <td>
-                            @if ($item->persona->estado == 1)
-                            <span class="badge rounded-pill text-bg-success">activo</span>
-                            @else
-                            <span class="badge rounded-pill text-bg-danger">eliminado</span>
-                            @endif
-                        </td>
-                        <td>
-                            <div class="d-flex justify-content-around">
+        <div class=" card-header">
+        <i class="fas fa-table me-1"></i>
+        Tabla proveedores
+    </div>
+    <div class="card-body">
+        <table id="datatablesSimple" class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Dirección</th>
+                    <th>Documento</th>
+                    <th>Tipo de persona</th>
+                    <th>Estado</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($proveedores as $item)
+                <tr>
+                    <td>
+                        {{$item->persona->razon_social}}
+                    </td>
+                    <td>
+                        {{$item->persona->direccion}}
+                    </td>
+                    <td>
+                        <p class="fw-semibold mb-1">{{$item->persona->documento->tipo_documento}}</p>
+                        <p class="text-muted mb-0">{{$item->persona->numero_documento}}</p>
+                    </td>
+                    <!-- <td>
+                        {{$item->persona->tipo_persona}}
+                    </td> -->
+                    <td>
+                        {{ $item->persona->tipo_persona === 'natural' ? 'Cliente particular' : 'Persona jurídica' }}
+                    </td>
+                    <td>
+                        @if ($item->persona->estado == 1)
+                        <span class="custom-badge">activo</span>
+                        @else
+                        <span class="custom-badge-delete">eliminado</span>
+                        @endif
+                    </td>
+ 
+                    <td>
+                            <div class="btn-group" role="group" aria-label="Basic mixed styles example">
 
-                                <div>
-                                    <button title="Opciones" class="btn btn-datatable btn-icon btn-transparent-dark me-2" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <svg class="svg-inline--fa fa-ellipsis-vertical" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="ellipsis-vertical" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512" data-fa-i2svg="">
-                                            <path fill="currentColor" d="M56 472a56 56 0 1 1 0-112 56 56 0 1 1 0 112zm0-160a56 56 0 1 1 0-112 56 56 0 1 1 0 112zM0 96a56 56 0 1 1 112 0A56 56 0 1 1 0 96z"></path>
-                                        </svg>
-                                    </button>
-                                    <ul class="dropdown-menu text-bg-light" style="font-size: small;">
-                                        <!-----Editar proveedore--->
-                                        @can('editar-proveedore')
-                                        <li><a class="dropdown-item" href="{{route('proveedores.edit',['proveedore'=>$item])}}">Editar</a></li>
-                                        @endcan
-                                    </ul>
-                                </div>
-                                <div>
-                                    <!----Separador----->
-                                    <div class="vr"></div>
-                                </div>
-                                <div>
-                                    <!------Eliminar proveedore---->
-                                    @can('eliminar-proveedore')
-                                    @if ($item->persona->estado == 1)
-                                    <button title="Eliminar" data-bs-toggle="modal" data-bs-target="#confirmModal-{{$item->id}}" class="btn btn-datatable btn-icon btn-transparent-dark">
-                                        <svg class="svg-inline--fa fa-trash-can" aria-hidden="true" focusable="false" data-prefix="far" data-icon="trash-can" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                                            <path fill="currentColor" d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"></path>
-                                        </svg>
-                                    </button>
-                                    @else
-                                    <button title="Restaurar" data-bs-toggle="modal" data-bs-target="#confirmModal-{{$item->id}}" class="btn btn-datatable btn-icon btn-transparent-dark">
-                                        <i class="fa-solid fa-rotate"></i>
-                                    </button>
-                                    @endif
-                                    @endcan
-                                </div>
+                                <form action="{{ route('proveedores.edit', ['proveedore' => $item]) }}" method="GET">
+                                    <!-- @csrf -->
+                                    <button type="submit" class="btn btn-pastel-yellow">Editar</button>
+                                </form>
+                                @if( $item->persona->estado == 1)
+                                <button type="button" class="btn btn-pastel-red" data-bs-toggle="modal" data-bs-target="#confirmModal-{{$item->id}}">Eliminar</button>
+                                @else
+                                <button type="button" class="btn btn-pastel-green" data-bs-toggle="modal" data-bs-target="#confirmModal-{{$item->id}}">Restaurar</button>
+                                @endif
                             </div>
-                        </td>
-                    </tr>
+                        <td>
 
-                    <!-- Modal de confirmación-->
-                    <div class="modal fade" id="confirmModal-{{$item->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Mensaje de confirmación</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    {{ $item->persona->estado == 1 ? '¿Seguro que quieres eliminar el proveedor?' : '¿Seguro que quieres restaurar el proveedor?' }}
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                    <form action="{{ route('proveedores.destroy',['proveedore'=>$item->persona->id]) }}" method="post">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button type="submit" class="btn btn-danger">Confirmar</button>
-                                    </form>
-                                </div>
+
+
+                </tr>
+
+                <!-- Modal de confirmación-->
+                <div class="modal fade" id="confirmModal-{{$item->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Mensaje de confirmación</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                {{ $item->persona->estado == 1 ? '¿Seguro que quieres eliminar el proveedor?' : '¿Seguro que quieres restaurar el proveedor?' }}
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                <form action="{{ route('proveedores.destroy',['proveedore'=>$item->persona->id]) }}" method="post">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-danger">Confirmar</button>
+                                </form>
                             </div>
                         </div>
                     </div>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                </div>
+                @endforeach
+            </tbody>
+        </table>
     </div>
+</div>
 </div>
 @endsection
 
 @push('js')
-    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" type="text/javascript"></script>
-    <script src="{{ asset('js/datatables-simple-demo.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" type="text/javascript"></script>
+<script src="{{ asset('js/datatables-simple-demo.js') }}"></script>
 @endpush
